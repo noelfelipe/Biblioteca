f01f15830157a6a75de7b3e485d5bf469666bd51
noel_ <noel_@NOEL>
2023-09-10 22:09:51 -0300

    adicionando metodo para paginação

--- Biblioteca.Application/LibraryService.cs
+++ Biblioteca.Application/LibraryService.cs
@@ -1,4 +1,4 @@
-﻿using Biblioteca.Application.Intefaces;
+using Biblioteca.Application.Intefaces;
 using Biblioteca.Domain;
 using Biblioteca.Domain.Intefaces;
 
@@ -6,37 +6,42 @@ namespace Biblioteca.Application
 {
     public class LibraryService : ILibraryService
     {
-  
-            private readonly ILivroRepository _livroRepository;
 
-            public LibraryService(ILivroRepository livroRepository)
-            {
-                _livroRepository = livroRepository;
-            }
+        private readonly ILivroRepository _livroRepository;
 
-            public async Task<IEnumerable<Livro>> ObterTodosLivrosAsync()
-            {
-                return await _livroRepository.ObterTodosAsync();
-            }
+        public LibraryService(ILivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
 
-            public async Task<Livro> ObterLivroPorIdAsync(int id)
-            {
-                return await _livroRepository.ObterPorIdAsync(id);
-            }
+        public async Task<IEnumerable<Livro>> ObterTodosLivrosAsync()
+        {
+            return await _livroRepository.ObterTodosAsync();
+        }
 
-            public async Task AdicionarLivroAsync(Livro livro)
-            {
-                await _livroRepository.AdicionarAsync(livro);
-            }
+        public async Task<Livro> ObterLivroPorIdAsync(int id)
+        {
+            return await _livroRepository.ObterPorIdAsync(id);
+        }
 
-            public async Task AtualizarLivroAsync(Livro livro)
-            {
-                await _livroRepository.AtualizarAsync(livro);
-            }
+        public async Task AdicionarLivroAsync(Livro livro)
+        {
+            await _livroRepository.AdicionarAsync(livro);
+        }
 
-            public async Task ExcluirLivroAsync(int id)
-            {
-                await _livroRepository.ExcluirAsync(id);
-            }
+        public async Task AtualizarLivroAsync(Livro livro)
+        {
+            await _livroRepository.AtualizarAsync(livro);
+        }
+
+        public async Task ExcluirLivroAsync(int id)
+        {
+            await _livroRepository.ExcluirAsync(id);
+        }
+
+        public async Task<IEnumerable<Livro>> ObterPaginadoAsync(int pagina, int tamanhoPagina)
+        {
+            return await _livroRepository.ObterPaginadoAsync(pagina, tamanhoPagina);
+        }
     }
 }
